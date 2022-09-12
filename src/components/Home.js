57e32073519f9a476968fb571922b6d6a7279b8c
Fred Denis <fredjamo68@gmail.com>
2022-09-12 13:21:01 +0300

    initial commit

---
+++ src/components/Home.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+function Home() {
+    return (
+        <ul className='inline-flex justify'>
+            <li>Github</li>
+            <li>Link</li>
+            <li>Tele</li>
+        </ul>
+    )
+}
+
+export default Home
